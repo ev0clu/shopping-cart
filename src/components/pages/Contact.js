c702984f28335e7f1866715c98d5f22ad2e0f232
ev0clu <ev0clu@gmail.com>
2023-04-07 21:41:00 +0200

    Contact page has prepared

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,9 +1,49 @@
 import React from 'react';
 
+import '../../assets/styles/Contact.css';
+import mapImg from '../../assets/images/map.png';
+import { BsInstagram, BsTwitter, BsMessenger } from 'react-icons/bs';
+
 const Contact = () => {
     return (
-        <div>
-            <h1>contact</h1>
+        <div className="contact">
+            <div className="contact-wrapper">
+                <div className="contact-info">
+                    <img src={mapImg} alt="map" />
+                </div>
+                <div className="contact-info">
+                    <p>Name:</p>
+                    <p>T-store Inc.</p>
+                </div>
+                <div className="contact-info">
+                    <p>Email:</p>
+                    <p>support@t-store.com</p>
+                </div>
+                <div className="contact-info">
+                    <p>Phone:</p>
+                    <p>+1 (555) 555-1234 </p>
+                </div>
+                <div className="contact-info">
+                    <p>Role:</p>
+                    <p>Customer Support</p>
+                </div>
+                <br />
+                <p>
+                    1234 Maple Avenue <br />
+                    Lakeview, CA 90210
+                </p>
+                <div className="contact-links">
+                    <a href="www.instagram.com">
+                        <BsInstagram />
+                    </a>
+                    <a href="www.twitter.com">
+                        <BsTwitter />
+                    </a>
+                    <a href="www.messenger.com">
+                        <BsMessenger />
+                    </a>
+                </div>
+            </div>
         </div>
     );
 };
